614bcf98dbde9c5382d9fcc5bd98002da4b289cf
jacopolegovini <legovini96@gmail.com>
2026-02-14 15:25:33 +0100

    create function to hide letters of the word

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -5,11 +5,45 @@ import { onMounted, ref } from 'vue';
 
 const word = ref<string>('');
 const definition = ref<string>('');
+const hiddenWord = ref<string>('');
 
+/**
+ * Permette di nascondere parti della parola
+ * @param word la parola selezionata da nascondere
+ * @returns {string[]} Un array di caratteri (es: ['a', '_', '_', 'e']).
+ */
+function hideRandomLetters(word: string) {
+    let wordArray: string[] = word.split('');
+    let numToHide: number = 0
+    let hiddenCount = 0;
+
+    if (word.length <= 4) {
+        numToHide = 3
+    } else if (word.length > 4 && word.length <= 8) {
+        numToHide = 5
+    }
+
+    while (hiddenCount < numToHide && hiddenCount < word.length) {
+        let randomIndex = Math.floor(Math.random() * wordArray.length);
+
+        if (wordArray[randomIndex] !== '_') {
+            wordArray.splice(randomIndex, 1, '_');
+            hiddenCount++;
+        }
+    }
+
+    return wordArray.join('');
+}
+
+/**
+ * Chiamata api che prende la definizione dal dizionario in base alla parola generata casualmente tramite il pacchetto random-words
+ */
 function getApi() {
     const newWord = generate() as string;
     word.value = newWord;
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${newWord}`;
+
+    hiddenWord.value = hideRandomLetters(newWord);
 
     axios.get(apiUrl)
         .then(response => {
@@ -30,7 +64,7 @@ onMounted(() => {
 
 <template>
     <section class="word">
-        <p>{{ word }}</p>
+        <p>{{ hiddenWord }}</p>
         <p>{{ definition }}</p>
         <button @click="getApi">Generate definition</button>
     </section>
